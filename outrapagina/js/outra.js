823ecebcb834ef9c5079f47ad2416f1d6a8c62c7
João Vitor da Silva Oliveira <jv7554817@gmail.com>
2023-04-16 01:52:13 -0300

    estou tentando fazer o display funcionar

--- outrapagina/js/outra.js
+++ outrapagina/js/outra.js
@@ -1,4 +1,19 @@
+
+
+
 function myFunction () {
     let teste = document.querySelector(".um");
     teste.style.display = "block";
+    
 }
+function myOtherFunction() {
+     
+    let background = document.querySelector(".dois");
+
+    background.style.display = "block";
+    }
+
+
+
+    
+    
