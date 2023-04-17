ee59b99e42a08bca924535a7a1f35d052fddd81d
João Vitor da Silva Oliveira <jv7554817@gmail.com>
2023-04-17 10:46:37 -0300

    quase finalizando

--- outrapagina/js/outra.js
+++ outrapagina/js/outra.js
@@ -2,9 +2,13 @@
 
 
 function myFunction () {
-    let teste = document.querySelector(".um");
-    teste.style.display = "block";
-    
+    let conversa = document.querySelector(".um");
+    conversa.style.display = "block";
+    let numero = document.querySelector("#nmensagem");
+    numero.style.display = "none";
+
+    let principal = document.querySelector(".figura");
+    principal.style.display = "none";
 }
 
 
